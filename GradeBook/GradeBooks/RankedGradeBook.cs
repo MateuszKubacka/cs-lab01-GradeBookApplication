11d5c7e59d09b8e34b1b8b93647813ff9bb4e21a
MateuszKubacka <mateusz.kubacka99@gmail.com>
2020-10-17 19:26:52 +0200

    Create RankedGradeBook.cs

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public String name { get; set; }
+        public GradeBookType Type = GradeBookType.Ranked;
+
+        public RankedGradeBook(string name, GradeBookType type) : base(name)
+        {
+            this.name = name;
+            Type = type;
+        }
+    }
+}
