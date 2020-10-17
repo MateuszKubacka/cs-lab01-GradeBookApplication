d139ff82f3fd0032029cb6fcfb622019d515c71f
MateuszKubacka <mateusz.kubacka99@gmail.com>
2020-10-17 19:24:44 +0200

    Create StandardGradeBook.cs

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook 
+    {
+        public String name { get; set; }
+
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.name = name;
+        }
+    }
+}
